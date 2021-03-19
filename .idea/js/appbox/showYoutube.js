77b867b1f8553ab11d98fa3bf183f040189c8868
hr <971021hr@naver.com>
2021-03-19 16:53:32 +0900

    21.03.19 hr.세미뱅 가짜 생일

--- .idea/js/appbox/showYoutube.js
+++ .idea/js/appbox/showYoutube.js
@@ -1,3 +1,15 @@
+$(document).ready(function (){
+
+});
+
 function showYoutube(){
     alert("유튜브 확인 함수 실행!");
+
+    $("#div_main").hide();
+
+    $("#div_youtube").show();
+    $("#div_subway").hide();
+    $("#div_bus").hide();
+
+
 }
